7b2ed2b3b8a2384139e8eeff1322057c87e4ca67
Kyle <adrianbrion4@gmail.com>
2025-04-20 18:13:48 +0800

    Setup SQLite database and added login and signup logic

--- server/backend/auth.py
+++ server/backend/auth.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, LoginManager
+from .db_models import User
+from.extensions import db
 
 auth = Blueprint('auth', __name__)
 
@@ -31,4 +35,10 @@ def signup():
         return redirect(url_for('auth.login'))
     
     return render_template('login.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
         
--- server/backend/extensions.py
+++ server/backend/extensions.py
@@ -1,10 +1,5 @@
-# WAG PANSININ TO, DITO KO LANG INIMPORT MGA KELANGAN, PARA LANG MALINIS YUNG FILE
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
-
-
 
 db = SQLAlchemy()
 
