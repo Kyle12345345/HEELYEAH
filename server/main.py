40ba5ed39d62cbea6f4d7d6ddaebaf58611404fc
Kyle <adrianbrion4@gmail.com>
2025-05-08 20:33:13 +0800

    set custom host and port for flask

--- server/main.py
+++ server/main.py
@@ -3,5 +3,6 @@ from backend import create_website, db
 app = create_website()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="192.168.0.17", port=1234) 
-    app.run(debug=True)
+    #app.run(debug=True, host="192.168.0.17", port=1234) 
+    app.run(debug=True, host="192.168.1.3", port=1234) 
+    #app.run(debug=True)
